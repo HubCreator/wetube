eca188de97f832ab0e039ba85b4a60ed8b6f7daa
HubCreator <chocomilk8604@gmail.com>
2021-01-15 20:53:08 +0900

    Install babel and use arrow function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+//const express = require('express')  // require은 node module을 어디로부터 가져오는 것
+import express from "express";
+const app = express()
+
+const PORT = 5000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send("hello from home");
+
+const handleProfile = (req, res) => res.send("You are on my profile");  // arrow function
+
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+
+app.listen(4000, handleListening);
